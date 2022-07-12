7d04779c6b4e64988570f27377776a1de3099e35
koladiyachintan <chintankoladiya4471@gmail.com>
2022-07-12 12:28:04 +0530

    initial commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,11 +10,19 @@ import {
 import { UserService } from './user.service';
 import * as bcrypt from 'bcrypt';
 import { User } from './user.model';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
+@ApiTags('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // @ApiOperation({ summary: 'List events' })
+  // @ApiResponse({ status: 200, description: 'Success', type: Events, isArray: true })
+  // @ApiInternalServerErrorResponse({ description: 'Internal server error' })
+  @ApiOperation({ summary: 'create User' })
+  @ApiResponse({ status: 200, description: 'Success', type: User })
+  @ApiBody({ type: User })
   @Post('/signin')
   async create(
     @Body('username') username: string,
@@ -23,29 +31,29 @@ export class UserController {
     const saltOrRounds = 10;
     const hashedpassword = await bcrypt.hash(password, saltOrRounds);
     const newuser = await this.userService.create(username, hashedpassword);
-    return {
-      msg: 'successfull',
-      userid: newuser.id,
-      username: newuser.username,
-    };
+    return newuser;
   }
 
+  @ApiOperation({ summary: 'get all user' })
   @Get('getall')
   findAll() {
     return this.userService.findAll();
   }
 
+  @ApiOperation({ summary: 'get user by id' })
   @Get('/:id')
   findbyid(@Param('id') id: string) {
     return this.userService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'update user by id' })
   @Put('/:id')
   update(@Param('id') id, @Body() username: User) {
     this.userService.update(id, username);
     return { msg: 'update successfull' };
   }
 
+  @ApiOperation({ summary: 'delete user by id' })
   @Delete('/:id')
   remove(@Param('id') id: string) {
     this.userService.remove(+id);
--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,19 +1,38 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
-export const UserSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-    },
-    password: {
-      type: String,
-    },
-  },
-  { timestamps: true },
-);
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
-export interface User extends mongoose.Document {
-  id: string;
+// export const UserSchema = new mongoose.Schema(
+//   {
+//     username: {
+//       type: String,
+//     },
+//     password: {
+//       type: String,
+//     },
+//   },
+//   { timestamps: true },
+// );
+
+// export interface User extends mongoose.Document {
+//   id: string;
+//   username: string;
+//   password: string;
+// }
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @ApiProperty()
+  @Prop({ required: true })
   username: string;
+
+  @ApiProperty()
+  @Prop({ required: true })
   password: string;
+
+  timestamps: true;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
